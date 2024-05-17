5efbb93b148af217d4316dbc2f3986bbc653e748
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2024-05-17 10:59:10 +0600

    done

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col gap-4">
       <div className="w-full lg:mt-[-10px] flex flex-col gap-4 lg:flex-row items-center">
-        <hr className="w-[95%] h-[1px] my-4 grad border-0 rounded" />
+        <hr className="w-[95%] h-[2px] my-4 grad border-0 rounded" />
         <div className="flex gap-2 lg:flex-col">
           <a href="mailto:info@QubitShield.io">
             <IoMail className="grad w-9 h-9 p-1 rounded-full text-black" />
