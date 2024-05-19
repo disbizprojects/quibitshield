bfb9cb7b0afeb8d23cf67853c16fb306f6ca27a2
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2024-05-19 07:49:13 +0600

    sdfsdf

--- components/footer.tsx
+++ components/footer.tsx
@@ -12,9 +12,9 @@ export default function Footer() {
           {" "}
           <hr className=" h-[2px] border-none bg-gradient-to-r from-[#1fdcbd] to-[#248ef5]"></hr>
         </div>
-        <div className="w-full flex justify-end mt-8">
-          <div>
-            <div className="flex  -mt-10 flex-end  w-full gap-4 flex-col">
+        <div className="w-full flex lg:justify-end  justify-center p-4 mt-8">
+          <div className="">
+            <div className="flex  -mt-10 lg:flex-end justify-center   w-full gap-4 flex-col">
               <div className="flex gap-2 items-center">
                 <IoMail className="grad w-9 h-9 p-1 rounded-full text-black" />
                 <a href="mailto:info@QubitShield.io">info@QubitShield.io</a>
