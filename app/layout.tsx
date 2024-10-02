3d7cd5afb8111577dd9afb206e091b93f7883fd9
AsirMahmud <email@asirmahmuhhdd@gamil.com>
2024-10-02 21:37:27 +0600

    sdfd

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,18 +3,52 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="   dark:bg-black">
+    <html lang="en" className="dark:bg-black">
       <SessionProvider>
-        <body className="font-[Axiforma-heavy] bg-black p-4  lg:py-2 lg:px-16">
+        <body className="font-[Axiforma-heavy] bg-black p-4 lg:py-2 lg:px-16">
           {children}
         </body>
         <Toaster />
+        <Script id="reb2b-script" strategy="afterInteractive">
+          {`
+            (function () {
+              var reb2b = window.reb2b = window.reb2b || [];
+              if (reb2b.invoked) return;
+              reb2b.invoked = true;
+              reb2b.methods = ["identify", "collect"];
+              reb2b.factory = function (method) {
+                return function () {
+                  var args = Array.prototype.slice.call(arguments);
+                  args.unshift(method);
+                  reb2b.push(args);
+                  return reb2b;
+                };
+              };
+              for (var i = 0; i < reb2b.methods.length; i++) {
+                var key = reb2b.methods[i];
+                reb2b[key] = reb2b.factory(key);
+              }
+              reb2b.load = function (key) {
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.async = true;
+                script.src = "https://s3-us-west-2.amazonaws.com/b2bjsstore/b/" + key + "/reb2b.js.gz";
+                var first = document.getElementsByTagName("script")[0];
+                first.parentNode.insertBefore(script, first);
+              };
+              reb2b.SNIPPET_VERSION = "1.0.1";
+              reb2b.load("GOYPYHV1LKOX");
+            })();
+          `}
+        </Script>
       </SessionProvider>
     </html>
   );
